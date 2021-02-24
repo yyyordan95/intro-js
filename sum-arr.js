342d27aa5ceecd0bd49f16e5d1e118bf52dd55b7
yyyordan95 <79213592+yyyordan95@users.noreply.github.com>
2021-02-24 16:33:24 +0200

    Create sum-arr.js

---
+++ sum-arr.js
@@ -0,0 +1,9 @@
+function sum(arr){
+    let num1 = Number(arr[0]);
+    let num2 = Number(arr[1]);
+    let num3 = Number(arr[2]);
+
+    let sum = num1 + num2 + num3;
+    console.log(sum);
+}
+sum(['2', '3', '4']);
